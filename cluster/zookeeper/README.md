3b8b741544bab58fa63aa387ca0f9323e4794501
Peter Suggitt <me@suggs.org.uk>
2021-11-11 07:53:36 +0000

    zookeeper updated

--- cluster/zookeeper/README.md
+++ cluster/zookeeper/README.md
@@ -1,1 +1,7 @@
-docker build -f Dockerfile -t suggitpe/zookeper
+# Some helpers
+
+## Build
+docker build -f Dockerfile -t local/zookeeper .
+
+## Run
+docker run -p 2181:2181 --net=host local/zookeeper
--- src/test/kotlin/org/suggs/sandbox/kafka/ConsumerStreamsSandboxTest.kt
+++ src/test/kotlin/org/suggs/sandbox/kafka/ConsumerStreamsSandboxTest.kt
@@ -1,4 +1,35 @@
 package org.suggs.sandbox.kafka
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.util.StdDateFormat
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.KeyValue
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.kstream.Consumed
+import org.apache.kafka.streams.kstream.KStream
+import org.junit.jupiter.api.Test
+import org.suggs.sandbox.kafka.domain.Person
+import java.time.ZoneId
+
 class ConsumerStreamsSandboxTest {
+
+    private val streamsBuilder = StreamsBuilder()
+    private val topic = "persons"
+    private val mapper = ObjectMapper().apply {
+        registerKotlinModule()
+        disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        dateFormat = StdDateFormat()
+    }
+
+    @Test fun `consumes from kafka as json from a stream`() {
+
+        val personJsonStream = streamsBuilder.stream<String, String>(topic, Consumed.with(Serdes.String(), Serdes.String()))
+        val personStream: KStream<String, String> = personJsonStream.mapValues { it -> mapper.readValue(it, Person::class.java) }
+        val resStream = personStream.map { _, p ->
+            val birthDate = p.birthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+            KeyValue("")
+        }
+    }
 }
